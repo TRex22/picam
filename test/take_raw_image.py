de6f2d23ed8780fbb4b6118b3a2eedbad0ff1494
TRex22 <contact@jasonchalom.com>
2020-11-21 22:03:07 +0200

    More additions and fixes :hammer:

--- test/take_raw_image.py
+++ test/take_raw_image.py
@@ -15,13 +15,11 @@ from picamera import PiCamera
 # 'bgra' - Write the raw image data to a file in 32-bit BGRA format
 # 'raw' - Deprecated option for raw captures; the format is taken from the deprecated raw_format attribute
 
-filetype = '.jpg'
-
-dcim_hdr_images_path = '/home/pi/DCIM/hdr_images'
+filetype = '.raw'
 dcim_images_path = '/home/pi/DCIM/images'
 dcim_videos_path = '/home/pi/DCIM/videos'
 
-existing_files = glob.glob(f'{dcim_hdr_images_path}/*{filetype}')
+existing_files = glob.glob(f'{dcim_images_path}/*{filetype}')
 filecount = len(existing_files)
 frame_count = filecount + 1
 
@@ -34,21 +32,7 @@ h = 2464
 
 camera.resolution = (w, h)
 
-# SEE: https://github.com/KEClaytor/pi-hdr-timelapse
-nimages = 10 #2160
-exposure_min = 10
-exposure_max = 90
-exp_step = 5
-
-exp_step = (exposure_max - exposure_min) / (nimages - 1.0)
-exposures = range(exposure_min, exposure_max + 1, int(exp_step))
-
-fnames = []
-for step in exposures:
-  # Set filename based on exposure
-  fname = f'{dcim_hdr_images_path}/{frame_count}_{step}_HDR_{filetype}' # 'e%d.jpg' % (step)
-  fnames.append(fname)
-  # Set camera properties and capture
-  camera.brightness = step
-  camera.capture(fname)
-
+camera.start_preview()
+# time.sleep(10)
+camera.capture(f'{dcim_images_path}/{frame_count}{filetype}')
+camera.stop_preview()
--- test/take_video.py
+++ test/take_video.py
@@ -11,10 +11,21 @@ existing_files = glob.glob(f'{dcim_videos_path}/*{filetype}')
 filecount = len(existing_files)
 frame_count = filecount + 1
 
+# Available formats: https://picamera.readthedocs.io/en/release-1.10/api_camera.html#picamera.camera.PiCamera.capture
+# 'h264' - Write an H.264 video stream
+# 'mjpeg' - Write an M-JPEG video stream
+# 'yuv' - Write the raw video data to a file in YUV420 format
+# 'rgb' - Write the raw video data to a file in 24-bit RGB format
+# 'rgba' - Write the raw video data to a file in 32-bit RGBA format
+# 'bgr' - Write the raw video data to a file in 24-bit BGR format
+# 'bgra' - Write the raw video data to a file in 32-bit BGRA format
+
+format = 'h264'
+
 camera = PiCamera()
 
 camera.start_preview()
-camera.start_recording(f'{dcim_videos_path}/{frame_count}{filetype}')
+camera.start_recording(f'{dcim_videos_path}/{frame_count}{filetype}', format=format)
 time.sleep(15)
 camera.stop_recording()
 camera.stop_preview()
