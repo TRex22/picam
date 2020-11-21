d5eb0ca34f2dce3b1f51aa07ab720b15beed81b3
TRex22 <contact@jasonchalom.com>
2020-11-21 21:48:10 +0200

    Fix filetypes :hammer:

--- test/take_hdr_image.py
+++ test/take_hdr_image.py
@@ -3,7 +3,19 @@ import glob
 
 from picamera import PiCamera
 
-filetype = '.tiff'
+# Supported file types: https://picamera.readthedocs.io/en/release-1.10/api_camera.html#picamera.camera.PiCamera.capture
+# 'jpeg' - Write a JPEG file
+# 'png' - Write a PNG file
+# 'gif' - Write a GIF file
+# 'bmp' - Write a Windows bitmap file
+# 'yuv' - Write the raw image data to a file in YUV420 format
+# 'rgb' - Write the raw image data to a file in 24-bit RGB format
+# 'rgba' - Write the raw image data to a file in 32-bit RGBA format
+# 'bgr' - Write the raw image data to a file in 24-bit BGR format
+# 'bgra' - Write the raw image data to a file in 32-bit BGRA format
+# 'raw' - Deprecated option for raw captures; the format is taken from the deprecated raw_format attribute
+
+filetype = '.jpeg'
 
 dcim_hdr_images_path = '/home/pi/DCIM/hdr_images'
 dcim_images_path = '/home/pi/DCIM/images'
@@ -36,3 +48,4 @@ for step in exposures:
   # Set camera properties and capture
   camera.brightness = step
   camera.capture(fname)
+
