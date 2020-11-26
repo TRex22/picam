6ebd238a60dc02a90a9924e57269a4ba8cd6d191
trex22 <contact@jasonchalom.com>
2020-11-26 23:29:50 +0200

    Cleanup :cop:

--- test/camera_test.py
+++ test/camera_test.py
@@ -103,6 +103,8 @@ button_4 = 17
 # GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setwarnings(True)
 # GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+
+GPIO.cleanup()
 GPIO.setmode(GPIO.BCM) # Use physical pin numbering
 
 GPIO.setup(button_1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
