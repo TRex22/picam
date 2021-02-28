3474efd58ce0dc982e75a47ab414fcfb69115d1a
TRex22 <contact@jasonchalom.com>
2021-02-28 19:30:29 +0200

    Add in global overlay variable :cop:

--- test/camera_test.py
+++ test/camera_test.py
@@ -267,12 +267,6 @@ button_4 = 17
 
 bouncetime = 300
 
-camera.resolution = (screen_w, screen_h)
-camera.start_preview()
-
-# camera.framerate = fps
-overlay = add_overlay(camera)
-
 # GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setwarnings(True)
 # GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
@@ -287,6 +281,13 @@ GPIO.add_event_detect(button_1, GPIO.RISING, callback=button_callback_1, bouncet
 GPIO.add_event_detect(button_2, GPIO.RISING, callback=button_callback_2, bouncetime=bouncetime)
 GPIO.add_event_detect(button_3, GPIO.RISING, callback=button_callback_3, bouncetime=bouncetime)
 GPIO.add_event_detect(button_4, GPIO.RISING, callback=button_callback_4, bouncetime=bouncetime)
+
+camera.resolution = (screen_w, screen_h)
+camera.start_preview()
+
+# camera.framerate = fps
+global overlay
+overlay = add_overlay(camera)
 
 message = input("Press enter to quit\n\n") # Run until someone presses enter
 camera.stop_preview()
