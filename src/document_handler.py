79fdfc04ae31d3a3971c188918dc775e542c257b
TRex22 <contact@jasonchalom.com>
2021-03-16 18:54:25 +0200

    Solve another bug :cop:

--- src/document_handler.py
+++ src/document_handler.py
@@ -24,7 +24,7 @@ def check_for_folders(config):
 def load_colour_profile(config):
   json_colour_profile = None
 
-  with open(colour_profile_path, "r") as file_stream:
+  with open(config["colour_profile_path"], "r") as file_stream:
     json_colour_profile = json.load(file_stream)
 
   return json_colour_profile
