423e5ecfd65d96129949425351faa4cc7690020a
TRex22 <contact@jasonchalom.com>
2021-04-03 11:15:19 +0200

    Add in other colour profile generation :hammer:

--- tools/process_raw_output.py
+++ tools/process_raw_output.py
@@ -1,6 +1,6 @@
 # TODO: Commandline inputs
 # Converts JPEGs with bayer EXIFdata into DNGs with camera profile applied
-VERSION = "0.0.1"
+VERSION = "0.0.2"
 
 import sys
 sys.path.insert(1, '../src/')
@@ -30,29 +30,33 @@ colour_profile_path = "/home/pi/Colour_Profiles/imx477/Raspberry Pi High Quality
 # colour_profile_path = "/home/pi/Colour_Profiles/imx477/Raspberry Pi High Quality Camera Lumariver 2860k-5960k Skin+Sky Look.json"
 # colour_profile_path = "/home/pi/Colour_Profiles/imx477/PyDNG_profile"
 
-print(f'Starting to convert original images to RAW with colour profile (Version: {VERSION})...')
-print(f'original_files_path: {original_files_path}')
-print(f'raw_file_save_path: {raw_file_save_path}\n')
+config = {
+  neutral_colour_profile: "/home/pi/Colour_Profiles/imx477/Raspberry Pi High Quality Camera Lumariver 2860k-5960k Neutral Look.json",
+  neutral_colour_profile_name: "neutral_colour",
+  skin_tone_colour_profile: "/home/pi/Colour_Profiles/imx477/Raspberry Pi High Quality Camera Lumariver 2860k-5960k Skin+Sky Look.json",
+  skin_tone_colour_profile_name: "skin_tone",
+  pydng_colour_profile: "/home/pi/Colour_Profiles/imx477/PyDNG_profile",
+  pydng_colour_profile_name: "pydng",
+  selected_colour_profile: "all" # can be all or neutral_colour_profile, skin_tone_colour_profile, pydng_colour_profile ... others to be added later
+}
 
-json_colour_profile = document_handler.load_colour_profile({ "colour_profile_path": colour_profile_path })
+def generate_filename(original_files_path, raw_file_save_path, f, config, colour_profile_name):
+  raw_file_save_path_with_profile = f'{raw_file_save_path}/{colour_profile_name}'
+  document_handler.detect_or_create_folder(raw_file_save_path_with_profile)
 
-document_handler.detect_or_create_folder(original_files_path)
-document_handler.detect_or_create_folder(raw_file_save_path)
-
-original_files = glob.glob(f'{original_files_path}/*')
-print(f'{len(original_files)} files to be processed.\n')
-
-global_start_time = time.time()
-
-for f in original_files:
-  filename = re.sub(original_files_path, raw_file_save_path, f)
+  filename = re.sub(original_files_path, raw_file_save_path_with_profile, f)
   filename = re.sub('.jpg', filetype, filename)
   filename = re.sub('.jpeg', filetype, filename)
 
+  return filename
+
+def convert_file(f, filename, config, colour_profile_name):
   print(f'{f} -> {filename}', end='')
+  start_time = time.time()
+
+  json_colour_profile = document_handler.load_colour_profile({ "colour_profile_path": config[colour_profile_name] })
 
   # Open file as a stream
-  start_time = time.time()
   stream = BytesIO()
 
   try:
@@ -69,6 +73,35 @@ for f in original_files:
     print(f' ({(time.time() - start_time)} seconds)')
   except:
     print(' ... failed, skipping file.')
+
+print(f'Starting to convert original images to RAW with colour profile (Version: {VERSION})...')
+print(f'original_files_path: {original_files_path}')
+print(f'raw_file_save_path: {raw_file_save_path}\n')
+
+document_handler.detect_or_create_folder(original_files_path)
+document_handler.detect_or_create_folder(raw_file_save_path)
+
+original_files = glob.glob(f'{original_files_path}/*')
+print(f'{len(original_files)} files to be processed.\n')
+
+global_start_time = time.time()
+
+for f in original_files:
+  colour_profile_name = config["selected_colour_profile"]
+
+  # TODO: improve and clean up this code
+  if (colour_profile_name == 'all' || colour_profile_name == "ALL"):
+    filename = generate_filename(original_files_path, raw_file_save_path, f, config, "neutral_colour_profile")
+    convert_file(f, filename, config, "neutral_colour_profile")
+
+    filename = generate_filename(original_files_path, raw_file_save_path, f, config, "skin_tone_colour_profile")
+    convert_file(f, filename, config, "skin_tone_colour_profile")
+
+    filename = generate_filename(original_files_path, raw_file_save_path, f, config, "pydng_colour_profile")
+    convert_file(f, filename, config, "pydng_colour_profile")
+  else:
+    filename = generate_filename(original_files_path, raw_file_save_path, f, config, colour_profile_name)
+    convert_file(f, filename, config, colour_profile_name)
 
 total_time = (time.time() - global_start_time)
 average_time = total_time / len(original_files)
