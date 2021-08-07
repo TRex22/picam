d25d8124bb21c4e32c7daa8d56a156729d0864b4
TRex22 <contact@jasonchalom.com>
2021-08-07 21:10:41 +0200

    Attempt to fix long exposure :wrench:

--- src/camera_handler.py
+++ src/camera_handler.py
@@ -74,6 +74,8 @@ def start_camera(original_config, skip_auto=False):
   overlay = None
 
   camera.resolution = (screen_w, screen_h)
+  camera.framerate = config["min_fps"]
+  camera.framerate = config["max_fps"]
   camera.framerate = screen_fps # fps
 
   overlay = overlay_handler.add_overlay(camera, overlay, config)
