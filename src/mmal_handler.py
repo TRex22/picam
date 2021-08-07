4b114612223be7857b73ade3fd429e09d0abf947
TRex22 <contact@jasonchalom.com>
2021-08-07 13:46:37 +0200

    Resolve pointer evil :cop:

--- src/mmal_handler.py
+++ src/mmal_handler.py
@@ -9,7 +9,7 @@ from picamerax.mmalobj import to_rational#, to_fraction, to_resolution
 
 def get_mmal_parameter(camera, parameter, type):
   if parameter == mmal.MMAL_PARAMETER_SHUTTER_SPEED:
-    value = mmal.MMAL_PARAMETER_UINT32_T
+    value = mmal.MMAL_RATIONAL_T
     ret = mmal.mmal_port_parameter_get_rational(camera._camera.control._port, parameter, value)
     print(f'MMAL GET Response: {ret}')
     return value
