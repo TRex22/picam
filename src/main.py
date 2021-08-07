93f795367d7061637ee19c9f40fd38994da88034
TRex22 <contact@jasonchalom.com>
2021-08-07 17:49:56 +0200

    Resolve fps 0.00 bug :cop:

--- src/main.py
+++ src/main.py
@@ -56,7 +56,7 @@ config = {
   "format": 'jpeg',
   "video_format": 'h264', # mjpeg, h264 TODO: Make into an option
   "bayer": True,
-  "fps": 0.001, # 1/200, # 60 # 10 fps max at full resolution
+  "fps": 0.01, # 1/200, # 60 # 10 fps max at full resolution
   "max_fps": 40, # Possibly equal to screen_fps
   "screen_fps": 40, # 120 fps at 1012x760
   "capture_timeout": 300, # Must be greater than max exposure in seconds
